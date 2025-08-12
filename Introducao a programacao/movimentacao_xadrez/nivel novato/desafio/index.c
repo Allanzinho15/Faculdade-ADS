c8d4c447ef41818368eb36f821b608799a492bd2
Allanzinho15 <allanhot2016@gmail.com>
2025-08-12 17:54:01 -0300

    atualizqaçao de um comenteria da movimentação das peças

--- Introducao a programacao/movimentacao_xadrez/nivel novato/desafio/index.c
+++ Introducao a programacao/movimentacao_xadrez/nivel novato/desafio/index.c
@@ -4,9 +4,10 @@ int main(){
     int torre = 0 , bispo , rainha = 0;
     int casatorre, casabispo, casarainha;
 
+    //usamos variavel como torre, bispo para inicializar a contagem das peças e variavel como casastorre e casabispo para determinar quantas vezes as peças iria andar no xadrex
 
     //Usamos o printf para pedir ao usuario quantas casas cada peça do xadrex vão se mover
-    
+
     //sacanf usamos para coletar o dados de quantas casas a peça do xadrez vai andar
 
     /*Usamos também atribuição com subtração, para quando o usuario colocasse quantas casas que ele queria que a peça andasse, contasse aparte do 1 e não do 0, ja que em programação o número ele sempre inicia do zero, porque usei isso,  porque se o usuario escolher que a peça deve andar 5 casas ele ande apenas as 5 e não 6 contando com 0, ex: sem atribuição com subtarção se o usuario pedir para que ele ande 5 casas o programa contaria asimm (0,1,2,3,4,5,) formando assim 6 casas, agora com atribuição com subtração se o usuario pedir para que ele ande 5 casas o programa ele anda apenas as 5 casas assim (1,2,3,4,5), explicando como funciona: quando o usuario pede para que a peça ande 5 casas, o que aconteçe, aconteçe que a variavel que recebe o numero de casas que a peça vai andar, a variavel recebe ela mesmo menos 1, exemplo: (casarainha = casarainha - 1 )
